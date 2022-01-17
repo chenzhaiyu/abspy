7c3a8a54e970e6259038c7fb5f812dfee154a726
Zhaiyu Chen <zhaiyu.chen@outlook.com>
2022-01-17 17:20:40 +0100

    Mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -59,3 +59,6 @@ html_theme = 'sphinx_rtd_theme'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# Mock imports
+autodoc_mock_imports = ['networkx', 'numpy', 'tqdm', 'scikit-learn', 'matplotlib', 'colorlog', 'scipy']
