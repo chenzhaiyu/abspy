3ea0ed8a6386accf3fa81292ed4a0818416dfd04
Zhaiyu Chen <zhaiyu.chen@outlook.com>
2022-01-19 20:49:37 +0100

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Zhaiyu Chen'
 author = 'Zhaiyu Chen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.4'
+release = '0.1.0'
 
 
 # -- General configuration ---------------------------------------------------
