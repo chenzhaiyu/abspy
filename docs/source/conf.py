75a4d1b8ebfa3e0e1cfcfa55538439e13893483e
chenzhaiyu <24312770+chenzhaiyu@users.noreply.github.com>
2022-02-20 17:05:21 +0100

    Bump version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Zhaiyu Chen'
 author = 'Zhaiyu Chen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
