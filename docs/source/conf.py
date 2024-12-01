a6e8f7363a258fecc0a028aa8866e57c0245a020
root <zhaiyu.chen@outlook.com>
2024-12-01 10:15:32 +0000

    Bump version to 0.2.5

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Zhaiyu Chen'
 author = 'Zhaiyu Chen'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.4'
+release = '0.2.5'
 
 
 # -- General configuration ---------------------------------------------------
